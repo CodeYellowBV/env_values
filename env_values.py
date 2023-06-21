73e923728dee862ccdf33c839d95869db42f8a2e
Gergely <gergely@codeyellow.nl>
2023-06-21 16:52:27 +0200

    Add JSON and YAML support

--- env_values.py
+++ env_values.py
@@ -1,20 +1,51 @@
+from dotenv import dotenv_values
 import os
-from dotenv import find_dotenv, dotenv_values
+import yaml
 
-# Find an environment file, either given by the env `CY_ENV_FILE` or find a file
-# called `.env`. Same for goes for an override file.
-CY_ENV_FILE = os.environ.get('CY_ENV_FILE', find_dotenv('.env'))
-CY_ENV_OVERRIDE_FILE = os.environ.get('CY_ENV_OVERRIDE_FILE', find_dotenv('.env.override'))
+def _gen_values(values, prefix=''):
+    for key, value in values.items():
+        yield (f'{prefix}{key}', value)
+        if isinstance(value, dict):
+            yield from _gen_values(value, prefix=f'{prefix}{key}_')
 
-def env_values():
-    """Retrieve environment variables by combining `os.environ`, `CY_ENV_FILE` and `CY_ENV_OVERRIDE_FILE`.
+def yaml_values(path):
+    """
+    Parse a JSON or YAML file and return its content as a dict.
+
+    The returned dict will contain all nested values on the
+    root level as well, identified by keys that are concatenations
+    of the keys of their ancestors.
+
+    For example, the following structure will produce four values:
+    `FOO`, `FOO_BAR`, `FOO_BAR_BAZ`, and `BAZ`.
+    ```
+    FOO:
+      BAR:
+        BAZ: baz
+    FOO_BAR: 1337
+    BAZ: ['foo', 'bar']
+    ```
+
+    Parameters:
+        path: Absolute or relative path to the file.
+    """
+    with open(path) as f:
+        values = yaml.safe_load(f)
+    if not isinstance(values, dict):
+        return {}
+    return dict(_gen_values(values))
+
+def env_values(*paths):
+    """Retrieve environment variables by combining `os.environ` and the provided env files.
 
     Get environment variables by looking at (in order):
         1. os.environ
-        2. .env.override
-        3. .env
+        2. file #n
+        3. file #n-1
+        ...
+      n+1. file #1
 
-    When a variable is present in `os.environ` and `.env`, the one in `.env` will be ignored and the one in `os.environ`
+    When a variable is present in `os.environ` and any of the files, the ones in the files will be ignored and the one in `os.environ`
     will "win".
 
     Calling `env_values()` has no side effects, eg `os.environ` not be altered.
@@ -22,18 +53,23 @@ def env_values():
     Returns:
         :dict: Env values.
 
-    Examples:
-        >>> env = env_values()
+    Example:
+        >>> env = env_values('.env')
         >>>
         >>> env.get('CY_DEBUG')
         >>> env.get('CY_DEBUG', False)
         >>> env['CY_DEBUG']
     """
-    env_file = dotenv_values(CY_ENV_FILE) if CY_ENV_FILE else {}
-    env_override_file = dotenv_values(CY_ENV_OVERRIDE_FILE) if CY_ENV_OVERRIDE_FILE else {}
-
+    values = {}
+    for path in paths:
+        if not path:
+            continue
+        if path.endswith('.json') or path.endswith('.yml') or path.endswith('.yaml'):
+            values.update(yaml_values(path))
+        else:
+            values.update(dotenv_values(path))
+    
     return {
-        **env_file,
-        **env_override_file,
-        **os.environ,
+        **values,
+        **os.environ
     }
